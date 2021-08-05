c0fe7073adac0442b5a0f9a37f92a24b922714f0
JT Black <jtblack.dev@gmail.com>
2021-08-05 16:18:13 +0100

    🚀  first front end commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+// modal handling
+// document.querySelector('#example-modal-button-2').addEventListener('click', function(e) {
+//     var modalTwo = Bulma('[data-modal="clickwindow"]').modal();
+//     modalTwo.open();
+// });
+
+
+
+
+
+
+
+
