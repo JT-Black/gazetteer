2b223145bd27c04d9116f99435d1c4952dd7a2c2
JT Black <jtblack.dev@gmail.com>
2021-08-06 12:00:42 +0100

    refactored id'a as classes, renamed variables

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mymap);
+}).addTo(map);
 
 // modal handling
 // document.querySelector('#example-modal-button-2').addEventListener('click', function(e) {
