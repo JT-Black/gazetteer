40c8f54701169cf62ebb81cd525f203b917bce89
JT Black <jtblack.dev@gmail.com>
2021-08-06 12:11:21 +0100

    fixed variable name

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
+var map = L.map('backgroundMap').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
